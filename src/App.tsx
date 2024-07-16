8f1474633c0a963372f8e5ce939ca67a6a41a30d
JoaoVitor <joaovitor2201@hotmail.com>
2024-07-15 21:34:18 -0300

    Criando design do projeto para mobile first

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,111 @@
-import React from 'react'
-import logo from './logo.svg'
+import { FaCirclePlus } from "react-icons/fa6";
 
-import { Container, Header, Logo } from './styles'
+import {
+  BackgroundProgress,
+  Container,
+  DataContainer,
+  DataContentContainer,
+  DataDescriptionContainer,
+  DayContainer,
+  DayDescription,
+  FilterButton,
+  FilterButtonContainer,
+  Icon,
+  InputContainer,
+  ItemContainer,
+  MonthDayDescription,
+  NewItemButton,
+  NewItemButtonContainer,
+  NewItemInputContainer,
+  ProgressBar,
+  StyledInput,
+  StyledInputContainer,
+  ToDoDescription,
+  ToDoItem,
+  YearDescription,
+} from "./styles";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  placeholder: string;
+  endIcon: boolean;
+}
+
+const Input: React.FC<InputProps> = ({ placeholder, endIcon, ...props }) => (
+  <StyledInputContainer>
+    <StyledInput {...props} placeholder={placeholder} />
+    {endIcon && <Icon />}
+  </StyledInputContainer>
+);
 
 function App() {
   return (
     <Container>
-      <Header>
-        <Logo src={logo} alt="Sizebay logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a href="https://sizebay.com" target="_blank" rel="noopener noreferrer">
-          Learn about Sizebay
-        </a>
-      </Header>
+      <DataContainer>
+        <DataContentContainer>
+          <div>
+            <DayDescription>07</DayDescription>
+          </div>
+          <DataDescriptionContainer>
+            <MonthDayDescription>Jul</MonthDayDescription>
+            <YearDescription>2021</YearDescription>
+          </DataDescriptionContainer>
+        </DataContentContainer>
+        <DayContainer>
+          <MonthDayDescription>Wednesday</MonthDayDescription>
+        </DayContainer>
+      </DataContainer>
+      <div>
+        <BackgroundProgress>
+          <ProgressBar />
+        </BackgroundProgress>
+      </div>
+      <InputContainer>
+        <Input placeholder="Search Items" endIcon />
+      </InputContainer>
+      <FilterButtonContainer>
+        <FilterButton>Done</FilterButton>
+        <FilterButton>Pending</FilterButton>
+      </FilterButtonContainer>
+      <NewItemInputContainer>
+        <StyledInput placeholder="Add new item..." className="newItem" />
+        <NewItemButtonContainer>
+          <NewItemButton>
+            <FaCirclePlus size="20px" color="#fff" />
+          </NewItemButton>
+        </NewItemButtonContainer>
+      </NewItemInputContainer>
+      <ItemContainer>
+        <ToDoItem>
+          <ToDoDescription>To DO ITEM 1</ToDoDescription>
+        </ToDoItem>
+        <ToDoItem>
+          <ToDoDescription>To DO ITEM 2</ToDoDescription>
+        </ToDoItem>
+        <ToDoItem>
+          <ToDoDescription>To DO ITEM 3</ToDoDescription>
+        </ToDoItem>
+        <ToDoItem>
+          <ToDoDescription>To DO ITEM 1</ToDoDescription>
+        </ToDoItem>
+        <ToDoItem>
+          <ToDoDescription>To DO ITEM 2</ToDoDescription>
+        </ToDoItem>
+        <ToDoItem>
+          <ToDoDescription>To DO ITEM 3</ToDoDescription>
+        </ToDoItem>
+        <ToDoItem>
+          <ToDoDescription>To DO ITEM 1</ToDoDescription>
+        </ToDoItem>
+        <ToDoItem>
+          <ToDoDescription>To DO ITEM 2</ToDoDescription>
+        </ToDoItem>
+        <ToDoItem>
+          <ToDoDescription>To DO ITEM 3</ToDoDescription>
+        </ToDoItem>
+      </ItemContainer>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- src/styles.ts
+++ src/styles.ts
@@ -1,21 +1,208 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { FaSearch } from "react-icons/fa";
 
 export const Container = styled.main`
-  text-align: center;
-`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 16px;
+  width: 341px;
+  height: 780px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border-radius: 4px;
+  opacity: 1;
+`;
 
-export const Logo = styled.img`
-  height: 10vmin;
-  pointer-events: none;
-`
+export const DataContainer = styled.div`
+  margin-top: 48px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0;
+`;
 
-export const Header = styled.div`
-  background-color: #4da6b4;
-  min-height: 100vh;
+export const DataContentContainer = styled.div`
+  display: flex;
+  justify-content: end;
+`;
+
+export const DayDescription = styled.span`
+  font-family: "Roboto", sans-serif;
+  font-weight: 500;
+  font-style: normal;
+
+  font-size: 60px;
+  color: #848484;
+  letter-spacing: 0;
+  line-height: 47px;
+`;
+
+export const DayContainer = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: flex-end;
+`;
+
+export const DataDescriptionContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
+`;
+
+export const MonthDayDescription = styled.span`
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+
+  font-size: 24px;
+  color: #848484;
+  letter-spacing: 0;
+  line-height: 22px;
+`;
+
+export const YearDescription = styled.span`
+  font-family: "Roboto", sans-serif;
+  font-weight: 300;
+  font-style: normal;
+
+  font-size: 24px;
+  color: #848484;
+  letter-spacing: 0;
+  line-height: 25px;
+`;
+
+export const BackgroundProgress = styled.div`
+  background: #e4e4e4 0% 0% no-repeat padding-box;
+  height: 24px;
+  width: 100%;
+  border-radius: 4px 0px 0px 4px;
+  opacity: 1;
+  margin: 32px 0 24px 0;
+`;
+
+export const ProgressBar = styled.div`
+  background: #5de290 0% 0% no-repeat padding-box;
   color: white;
-`
+  text-align: right;
+  font-size: 20px;
+  border-radius: 4px 0px 0px 4px;
+
+  height: 24px;
+
+  width: 80%;
+  opacity: 1;
+`;
+
+export const StyledInputContainer = styled.div`
+  position: relative;
+  display: grid;
+`;
+
+export const NewItemInputContainer = styled.div`
+  margin-top: 16px;
+  display: flex;
+  width: 100%;
+`;
+
+export const StyledInput = styled.input`
+  padding: 0 16px;
+  outline: none;
+  height: 48px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 1px solid #dbdbdb;
+  border-radius: 4px;
+  text-align: left;
+  font: normal normal normal 14px/19px Roboto;
+  letter-spacing: 0px;
+  color: #848484;
+  padding-right: 40px;
+
+  &.newItem {
+    width: 100%;
+    border-radius: 4px 0px 0px 4px;
+    border-right: none;
+  }
+`;
+
+export const Icon = styled(FaSearch)`
+  font: 14px/19px;
+  position: absolute;
+  right: 16px;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+  color: #848484;
+`;
+
+export const InputContainer = styled.div`
+  margin-bottom: 10px;
+`;
+
+export const FilterButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const FilterButton = styled.button`
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 1px solid #dbdbdb;
+  border-radius: 17px;
+  opacity: 1;
+  text-align: left;
+  font: normal normal normal 14px/19px Roboto;
+  letter-spacing: 0px;
+  color: #848484;
+  opacity: 1;
+  padding: 8px 16px 6px 16px;
+`;
+
+export const NewItemButtonContainer = styled.div`
+  background: #4da6b3 0% 0% no-repeat padding-box;
+  border: 1px solid #4da6b3;
+  border-radius: 0 4px 4px 0;
+  margin-bottom: 16px;
+`;
+
+export const NewItemButton = styled.button`
+  border: none;
+  background: #4da6b3 0% 0% no-repeat padding-box;
+  border-radius: 0px 4px 4px 0px;
+  opacity: 1;
+  height: 48px;
+  width: 44px;
+`;
+
+export const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  scrollbar-width: thin;
+  max-height: 400px;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  scrollbar-width: none;
+
+  -ms-overflow-style: none;
+`;
+
+export const ToDoItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  height: 48px;
+  background: #f4f4f4 0% 0% no-repeat padding-box;
+  border: 1px solid #dbdbdb;
+  opacity: 1;
+  margin-bottom: 8px;
+  flex-shrink: 0;
+`;
+
+export const ToDoDescription = styled.span`
+  text-align: left;
+  font: normal normal normal 14px/19px Roboto;
+  letter-spacing: 0px;
+  color: #848484;
+  opacity: 1;
+`;
+
